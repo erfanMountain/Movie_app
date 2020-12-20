c481abcfa969a6a0d72757cc357c378bfe393b96
erfan <ahmederfan822@gmail.com>
2020-12-20 10:30:30 +0200

    edit model->main

--- movies/urls.py
+++ movies/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import home_page, create
+from .views import home_page, create, edit
 
 
 urlpatterns = [
     path('', home_page, name='home_page'),
-    path('create/', create, name='create')
+    path('create/', create, name='create'),
+    # Here <str:movie_id> tell us about the movie id
+    # Here name attribute is the friendly name what I will use in the template
+    path('edit/<str:movie_id>', edit, name='edit')
 ]
